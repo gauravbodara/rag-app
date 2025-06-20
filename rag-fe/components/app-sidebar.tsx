308714f9eb69b0480e9be8aa56270723a8ae2301
Gaurav Bodara <gauravbodra20@gmail.com>
2025-06-20 15:07:53 +0530

    frontend cleanup

--- rag-fe/components/app-sidebar.tsx
+++ rag-fe/components/app-sidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Github, MessagesSquare } from "lucide-react"
+import { MessagesSquare } from "lucide-react"
 import Link from "next/link"
 import {
   Sidebar,
@@ -20,12 +20,12 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarMenu>
           <SidebarMenuItem>
             <SidebarMenuButton size="lg" asChild>
-                <Link href="https://assistant-ui.com" target="_blank">
+                <Link href="/" target="_blank">
                   <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
                     <MessagesSquare className="size-4" />
                   </div>
                   <div className="flex flex-col gap-0.5 leading-none">
-                    <span className="font-semibold">assistant-ui</span>
+                    <span className="font-semibold">RAG Assistant</span>
                   </div>
                 </Link>
               </SidebarMenuButton>
@@ -39,21 +39,6 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       <SidebarRail />
       <SidebarFooter>
         <SidebarMenu>
-         
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <Link href="https://github.com/assistant-ui/assistant-ui" target="_blank">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <Github className="size-4" />
-                </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">GitHub</span>
-                  <span className="">View Source</span>
-                </div>
-              </Link>
-            </SidebarMenuButton>
-            
-          </SidebarMenuItem>
         </SidebarMenu>
       </SidebarFooter>
     </Sidebar>
